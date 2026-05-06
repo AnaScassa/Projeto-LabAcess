341d7b8e8188d7fb6a0adaaf9764bfae1020e442
anascassa <anacaroline25190@gmail.com>
2026-05-06 13:32:29 -0300

    changing login

--- users/rabbitmq/consumer.py
+++ users/rabbitmq/consumer.py
@@ -1,20 +1,79 @@
-#producer e um consumer
-
+import django
 import pika
+import json
+import requests
+import time
+import sys
+import os
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='rabbitmq')
+sys.path.append('/app')
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'users_service.settings'
 )
+
+import django
+django.setup()
+
+from django.core.cache import cache
+
+connection = None
+
+while connection is None:
+    try:
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='rabbitmq')
+        )
+
+        print("Conectado ao RabbitMQ!")
+
+    except Exception as e:
+        print("Aguardando RabbitMQ...", e)
+        time.sleep(5)
 
 channel = connection.channel()
 
-channel.queue_declare(queue='usuarios')
+channel.queue_declare(queue='usuarios_processados')
 
 def callback(ch, method, properties, body):
-    print("Mensagem recebida:", body.decode())
+
+    try:
+        data = json.loads(body)
+
+        task_id = data["task_id"]
+
+        print("Recebido task_id:", task_id)
+
+        headers = {
+            "X-Api-Key": "pbkdf2_sha256$1000000$EaYqRbLmLW9yWEEFxzLD7G$3OxMES/nb5+z6zqCtA9UmDKRGWvLL0Fp46KMdR5CEJY=",
+            "Authorization": f"Api-Key Nq5UAGLV.YuZjPXxyvJ1kclWNprTeIxPTAZcqnhza"
+        }
+        
+        print(headers)
+
+        profiles = requests.get(
+            "http://users_service:8001/api/users/user-profile/",
+            headers=headers,
+            timeout=10
+        ).json()
+
+        users = requests.get(
+            "http://users_service:8001/api/users/user/",
+            headers=headers,
+            timeout=10
+        ).json()
+
+        cache.set(f"profiles_{task_id}", profiles, timeout=600)
+        cache.set(f"users_{task_id}", users, timeout=600)
+
+        print("Dados salvos no cache!")
+
+    except Exception as e:
+        print("ERRO NO CONSUMER:", e)
 
 channel.basic_consume(
-    queue='usuarios',
+    queue='usuarios_processados',
     on_message_callback=callback,
     auto_ack=True
 )
@@ -22,3 +81,5 @@ channel.basic_consume(
 print("Aguardando mensagens...")
 
 channel.start_consuming()
+
+print("Fim do consumer")
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,16 @@
-from rest_framework.routers import DefaultRouter
+from django.urls import include
+
+from rest_framework.routers import DefaultRouter, path
 from .views import UserViewSet, UserProfileViewSet, UsersSafetyTraining
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r"user", UserViewSet, basename="user")
 router.register(r"user-profile", UserProfileViewSet, basename="user-profile")
 router.register(r"safety-training", UsersSafetyTraining, basename="safety-training")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("", include(router.urls)),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
