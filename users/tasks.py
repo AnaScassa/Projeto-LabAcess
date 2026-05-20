cc56113b7cc143ebfe89f59fb6841e5fe805953e
AnaScassa <anacaroline25190@gmail.com>
2026-05-20 13:33:42 -0300

    messages

--- users/tasks.py
+++ users/tasks.py
@@ -2,83 +2,54 @@ import threading
 import pika
 import json
 import time
-from celery import shared_task
 from django.contrib.auth import get_user_model
 from users.models import UserProfile
-from django.core.serializers.json import DjangoJSONEncoder
 
 User = get_user_model()
 
 
-@shared_task
-def processar_usuarios(task_id):
-    try:
-        users = list(User.objects.all().values())
-        profiles = list(UserProfile.objects.all().values())
-
-        response = {
-            "task_id": task_id,
-            "users": users,
-            "profiles": profiles
-        }
-
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host="rabbitmq")
-        )
-
-        channel = connection.channel()
-        channel.queue_declare(queue="usuarios_resposta", durable=True)
-        channel.basic_publish(
-            exchange='',
-            routing_key='usuarios_resposta',
-            body=json.dumps(response, cls=DjangoJSONEncoder)
-        )
-        connection.close()
-
-        print(f"Resposta enviada para task_id: {task_id}")
-
-    except Exception as e:
-        print(f"ERRO ao processar usuarios: {str(e)}")
-        raise
-
 def iniciar_consumer():
+    
     connection = None
 
     while connection is None:
         try:
-            connection = pika.BlockingConnection(
-                pika.ConnectionParameters(host="rabbitmq")
-            )
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
             print("Conectado RabbitMQ")
+
         except Exception as e:
             print(f"Aguardando RabbitMQ... {e}")
             time.sleep(5)
 
     channel = connection.channel()
-    channel.queue_declare(queue="usuarios_processados", durable=True)
-    channel.queue_declare(queue="usuarios_resposta", durable=True)
+    channel.queue_declare(queue='usuarios_processados', durable=True)
 
     def callback(ch, method, properties, body):
+
         try:
             data = json.loads(body)
             task_id = data["task_id"]
+            
+            users = list(User.objects.all().values())
+            profiles = list(UserProfile.objects.all().values())
 
-            print(f"Recebido task_id: {task_id}")
+            resposta = {
+                "users": users,
+                "profiles": profiles,
+                "status": "success"
+            }
 
-            processar_usuarios.delay(task_id)
-
-            print(f"Tarefa Celery disparada para task_id: {task_id}")
+            ch.basic_publish(exchange='', routing_key=properties.reply_to, properties=pika.BasicProperties
+                (correlation_id=properties.correlation_id), body=json.dumps(resposta, default=str))
+            ch.basic_ack(delivery_tag=method.delivery_tag)
 
         except Exception as e:
             print(f"ERRO: {str(e)}")
+            resposta = {
+                "status": "error",
+            }
 
-    channel.basic_consume(
-        queue="usuarios_processados",
-        on_message_callback=callback,
-        auto_ack=True
-    )
-
-    print("Aguardando mensagens do smartcard...")
+    channel.basic_consume(queue='usuarios_processados', on_message_callback=callback)
     channel.start_consuming()
 
 consumer_thread = threading.Thread(target=iniciar_consumer, daemon=True)
