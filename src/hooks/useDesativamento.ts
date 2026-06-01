2d08f9640fc51a89cc1883f7b477853ae5d0c122
anascassa <anacaroline25190@gmail.com>
2026-06-01 10:11:22 -0300

    adding buttons

--- src/hooks/useDesativamento.ts
+++ src/hooks/useDesativamento.ts
@@ -5,8 +5,6 @@ export const useApontamentoActions = () => {
   const handleApontamento = async (id: number) => {
     try {
       await desativarApontamento(id);
-      alert("Registro apagado!");
-      window.location.reload();
     } catch (error) {
       console.log("Erro ao excluir apontamento", error);
     }
